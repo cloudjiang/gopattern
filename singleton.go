91a223e4fdbb4b6ac8efa73298928722d62afba5
cloudjiang <jackson.ina@gmail.com>
2015-12-31 12:36:35 +0800

    cr

---
+++ singleton.go
@@ -0,0 +1,16 @@
+﻿package pattern
+
+import "sync"
+
+type Singleton struct {
+	once sync.Once
+	Inst interface{}
+	newFunc func() interface{}
+}
+
+func (this *Ston)GetInstance() interface{} {
+	this.once.Do(func() {
+		this.Inst = this.newFunc()
+	})
+	return this.Inst
+}
